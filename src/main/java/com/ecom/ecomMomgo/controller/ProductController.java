6b56c981415e9bc4b43e63e144f0aea5edef18c4
situparna panda <situparna.panda@capgemini.com>
2020-10-11 16:52:17 +0530

    jar packaging and added missing field
    jar packaging and added missing field

--- src/main/java/com/ecom/ecomMomgo/controller/ProductController.java
+++ src/main/java/com/ecom/ecomMomgo/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.ecomMomgo.services.ProductServices;
@@ -24,6 +25,7 @@ import com.ecom.ecomMongo.repository.ProductRepository;
  */
 
 @RestController
+@RequestMapping("/v1")
 public class ProductController {
 
 	@Autowired
--- src/main/java/com/ecom/ecomMongo/model/Product.java
+++ src/main/java/com/ecom/ecomMongo/model/Product.java
@@ -85,6 +85,10 @@ public class Product {
 	
 	public int size;
 	
+	public int length;
+	
+	public String color;
+	
 	public String img1;
 	
 	public String img2;
@@ -93,9 +97,363 @@ public class Product {
 	
 	public String img4;
 	
+	public String img5;
+	
 	public int prodRefId;
 	
-	 @Override
+	 public int getRowIndex() {
+		return rowIndex;
+	}
+
+	public void setRowIndex(int rowIndex) {
+		this.rowIndex = rowIndex;
+	}
+
+	public String getPrdName() {
+		return prdName;
+	}
+
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getModelNumber() {
+		return modelNumber;
+	}
+
+	public void setModelNumber(int modelNumber) {
+		this.modelNumber = modelNumber;
+	}
+
+	public String getFrdURL() {
+		return frdURL;
+	}
+
+	public void setFrdURL(String frdURL) {
+		this.frdURL = frdURL;
+	}
+
+	public String getSkuCode() {
+		return skuCode;
+	}
+
+	public void setSkuCode(String skuCode) {
+		this.skuCode = skuCode;
+	}
+
+	public String getPrdDesc() {
+		return prdDesc;
+	}
+
+	public void setPrdDesc(String prdDesc) {
+		this.prdDesc = prdDesc;
+	}
+
+	public String getOccasion() {
+		return occasion;
+	}
+
+	public void setOccasion(String occasion) {
+		this.occasion = occasion;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public boolean isKids() {
+		return kids;
+	}
+
+	public void setKids(boolean kids) {
+		this.kids = kids;
+	}
+
+	public int getMinAge() {
+		return minAge;
+	}
+
+	public void setMinAge(int minAge) {
+		this.minAge = minAge;
+	}
+
+	public int getMaxAge() {
+		return maxAge;
+	}
+
+	public void setMaxAge(int maxAge) {
+		this.maxAge = maxAge;
+	}
+
+	public boolean isSelfInvMgt() {
+		return selfInvMgt;
+	}
+
+	public void setSelfInvMgt(boolean selfInvMgt) {
+		this.selfInvMgt = selfInvMgt;
+	}
+
+	public boolean isItemReturn() {
+		return itemReturn;
+	}
+
+	public void setItemReturn(boolean itemReturn) {
+		this.itemReturn = itemReturn;
+	}
+
+	public boolean isItemCancel() {
+		return itemCancel;
+	}
+
+	public void setItemCancel(boolean itemCancel) {
+		this.itemCancel = itemCancel;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public int getQtyForOOSS() {
+		return qtyForOOSS;
+	}
+
+	public void setQtyForOOSS(int qtyForOOSS) {
+		this.qtyForOOSS = qtyForOOSS;
+	}
+
+	public int getMinQtyForCart() {
+		return minQtyForCart;
+	}
+
+	public void setMinQtyForCart(int minQtyForCart) {
+		this.minQtyForCart = minQtyForCart;
+	}
+
+	public int getMaxQtyForCart() {
+		return maxQtyForCart;
+	}
+
+	public void setMaxQtyForCart(int maxQtyForCart) {
+		this.maxQtyForCart = maxQtyForCart;
+	}
+
+	public int getNotifyQty() {
+		return notifyQty;
+	}
+
+	public void setNotifyQty(int notifyQty) {
+		this.notifyQty = notifyQty;
+	}
+
+	public String getShipping() {
+		return shipping;
+	}
+
+	public void setShipping(String shipping) {
+		this.shipping = shipping;
+	}
+
+	public String getShpTerms() {
+		return shpTerms;
+	}
+
+	public void setShpTerms(String shpTerms) {
+		this.shpTerms = shpTerms;
+	}
+
+	public int getShpCharges() {
+		return shpCharges;
+	}
+
+	public void setShpCharges(int shpCharges) {
+		this.shpCharges = shpCharges;
+	}
+
+	public String getPriceType() {
+		return priceType;
+	}
+
+	public void setPriceType(String priceType) {
+		this.priceType = priceType;
+	}
+
+	public float getMrp() {
+		return mrp;
+	}
+
+	public void setMrp(float mrp) {
+		this.mrp = mrp;
+	}
+
+	public float getListPrice() {
+		return listPrice;
+	}
+
+	public void setListPrice(float listPrice) {
+		this.listPrice = listPrice;
+	}
+
+	public String getCurrencyType() {
+		return currencyType;
+	}
+
+	public void setCurrencyType(String currencyType) {
+		this.currencyType = currencyType;
+	}
+
+	public String getTaxType() {
+		return taxType;
+	}
+
+	public void setTaxType(String taxType) {
+		this.taxType = taxType;
+	}
+
+	public float getTaxPercentage() {
+		return taxPercentage;
+	}
+
+	public void setTaxPercentage(float taxPercentage) {
+		this.taxPercentage = taxPercentage;
+	}
+
+	public boolean isTaxInclusive() {
+		return taxInclusive;
+	}
+
+	public void setTaxInclusive(boolean taxInclusive) {
+		this.taxInclusive = taxInclusive;
+	}
+
+	public boolean isGiftWrapAvl() {
+		return giftWrapAvl;
+	}
+
+	public void setGiftWrapAvl(boolean giftWrapAvl) {
+		this.giftWrapAvl = giftWrapAvl;
+	}
+
+	public float getGiftWrapAmt() {
+		return giftWrapAmt;
+	}
+
+	public void setGiftWrapAmt(float giftWrapAmt) {
+		this.giftWrapAmt = giftWrapAmt;
+	}
+
+	public boolean isCodAvl() {
+		return codAvl;
+	}
+
+	public void setCodAvl(boolean codAvl) {
+		this.codAvl = codAvl;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getImg1() {
+		return img1;
+	}
+
+	public void setImg1(String img1) {
+		this.img1 = img1;
+	}
+
+	public String getImg2() {
+		return img2;
+	}
+
+	public void setImg2(String img2) {
+		this.img2 = img2;
+	}
+
+	public String getImg3() {
+		return img3;
+	}
+
+	public void setImg3(String img3) {
+		this.img3 = img3;
+	}
+
+	public String getImg4() {
+		return img4;
+	}
+
+	public void setImg4(String img4) {
+		this.img4 = img4;
+	}
+
+	public String getImg5() {
+		return img5;
+	}
+
+	public void setImg5(String img5) {
+		this.img5 = img5;
+	}
+
+	public int getProdRefId() {
+		return prodRefId;
+	}
+
+	public void setProdRefId(int prodRefId) {
+		this.prodRefId = prodRefId;
+	}
+
+	@Override
 	    public String toString() {
 	        return "Product{" +
 	                "rowIndex=" + rowIndex +
